358d7872b7a7e89b82ba637797415c50c476978d
dorayo <yomingyo@gmail.com>
2016-01-14 18:28:31 +0800

    first init

---
+++ LG_runTime运行时改变UI/CALayerCateGories.swift
@@ -0,0 +1,21 @@
+//
+//  CALayerCateGories.swift
+//  LG_runTime运行时改变UI
+//
+//  Created by 孙雷钢 on 16/1/14.
+//  Copyright © 2016年 carme. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+extension CALayer {
+	
+	func setBorderColorFromUIColor(color:UIColor ){
+	  self.borderColor = color.CGColor
+	}
+	
+
+}
